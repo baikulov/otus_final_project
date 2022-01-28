617b2eff85efcedcadc5f122a33be19c431a5c47
baikulov <baikulov.ant@gmail.com>
2022-01-28 17:56:43 +0400

    fix sudo

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-sudo apt-get update
-sudo apt-get install git -y
+#!/bin/bash
+apt-get update 
+apt-get install git -y
 git clone https://github.com/baikulov/shiny_bigquery_monitoring.git
 cd shiny_bigquery_monitoring
